9e69d29947c4e8453c8a08e2a0e5b7e863225d38
Taha Bohra <tahamustafa053@gmail.com>
2021-01-20 12:39:40 +0530

    modified => booking form, drawer icons

--- app/routes.js
+++ app/routes.js
@@ -78,7 +78,9 @@ router.get('/taxi-booking/*', function (req, res) {
         page_offer_description: pageData.page_offer_description,
         page: 'index',
         total_cars: _.groupBy(_.sortBy(carDetailsData, 'car_type'), 'car_type'),
-        total_routes: routeData
+        total_routes: routeData,
+        from: "From",
+        to: "To"
       });
     }).catch((error) => {
       res.render('under-maintenance.ejs');
